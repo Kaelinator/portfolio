c2ac7a4099244861375ad3b3e6a025a416edbfbd
Kaelinator <kirkkael@gmail.com>
2018-07-12 02:48:02 -0500

    added rockwell network data

--- src/meta/coding-data.js
+++ src/meta/coding-data.js
@@ -1,4 +1,8 @@
 
+import rockwellIcon from '../images/rockwell-network/rockwell-icon.png'
+import rockwellHomepage from '../images/rockwell-network/homepage.png'
+import rockwellLevels from '../images/rockwell-network/levels.png'
+
 export default {
   title: "Coding",
   color: "#5eaa4f",
@@ -7,21 +11,29 @@ export default {
   cards: [
     {
       title: 'Rockwell server network',
-      alt: 'Judy hopps',
-      description: 'This was a complete Minecraft server network that I created in the summer of 2016. Including two minigames, leaderboards, and ranks.',
-      previewImg: 'https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fimages6.fanpop.com%2Fimage%2Fphotos%2F39900000%2Fjudy-hopps-judy-hopps-39913646-750-750.jpg&f=1',
+      alt: 'server icon',
+      description: 'This was a complete Minecraft server network that I created in the summer of 2016. Including two minigames, online leaderboards, and ranks. By utilizing PHP, MySQL, and PayPal\'s REST API, I was able to create an online shop from scratch.',
+      previewImg: rockwellIcon,
       carousel: [
         {
-          description: 'It was really cool!',
-          image: 'https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fimages6.fanpop.com%2Fimage%2Fphotos%2F39600000%2FJudy-Hopps-disneys-zootopia-39651126-960-960.jpg&f=1'
+          description: 'Lobby built by IanDragonMaster for UHC-In-A-Box, a minigame resembling today\'s popular UHC Meetup minigame',
+          image: 'https://pbs.twimg.com/media/CxwZu_OUUAEDwFE.jpg:large'
         },
         {
-          description: 'Another really cool description!',
-          image: 'https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fimages6.fanpop.com%2Fimage%2Fphotos%2F39600000%2Fzootopia-judy-hopps-zootopia-39638111-1271-792.jpg&f=1'
+          description: 'Debugging the player spawnpoints in UHC-In-A-Box',
+          image: 'https://pbs.twimg.com/media/CqA_aQkUsAAlqwX.jpg:large'
         },
         {
-          description: 'It was really neat!',
-          image: 'https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Forig14.deviantart.net%2Fd7a5%2Ff%2F2016%2F123%2F0%2F9%2Fjudy_and_nick_by_kuvshinov_ilya-da15pnt.jpg&f=1'
+          description: 'Map voting system for Levels minigame',
+          image: 'https://pbs.twimg.com/media/C0V5UEeVIAA7k1W.jpg:large'
+        },
+        {
+          description: 'Home page, created with PHP',
+          image: rockwellHomepage
+        },
+        {
+          description: 'Levels minigame lobby',
+          image: rockwellLevels
         }
       ]
     }
