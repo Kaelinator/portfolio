0bcbafa3c58eb081aa213b0a32f5a449003d98b5
Kaelinator <kirkkael@gmail.com>
2018-07-19 23:12:03 -0500

    clipping background works on chrome

--- src/meta/creating-data.js
+++ src/meta/creating-data.js
@@ -4,26 +4,5 @@ export default {
   color: "#ffc145", 
   description: "I spend the greater portion of my freetime coding. I love to incorporate code wherever I can, whether into school, internships, and running.",
   icon: "video-camera",
-  cards: [
-    {
-      title: 'Rockwell server network',
-      alt: 'Judy hopps',
-      description: 'This was a complete Minecraft server network that I created in the summer of 2016. Including two minigames, leaderboards, and ranks.',
-      previewImg: 'https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fimages6.fanpop.com%2Fimage%2Fphotos%2F39900000%2Fjudy-hopps-judy-hopps-39913646-750-750.jpg&f=1',
-      carousel: [
-        {
-          description: 'It was really cool!',
-          image: 'https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fimages6.fanpop.com%2Fimage%2Fphotos%2F39600000%2FJudy-Hopps-disneys-zootopia-39651126-960-960.jpg&f=1'
-        },
-        {
-          description: 'Another really cool description!',
-          image: 'https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fimages6.fanpop.com%2Fimage%2Fphotos%2F39600000%2Fzootopia-judy-hopps-zootopia-39638111-1271-792.jpg&f=1'
-        },
-        {
-          description: 'It was really neat!',
-          image: 'https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Forig14.deviantart.net%2Fd7a5%2Ff%2F2016%2F123%2F0%2F9%2Fjudy_and_nick_by_kuvshinov_ilya-da15pnt.jpg&f=1'
-        }
-      ]
-    }
-  ]
+  cards: []
 }
