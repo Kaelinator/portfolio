2d8ec0d5894125edd41c0acd1883f1a2460a4c70
Kaelinator <kirkkael@gmail.com>
2018-12-28 15:19:15 -0600

    react-pose + updates

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './routes/Home';
 
+const ToDo = () => <h1>Under construction</h1>;
+
 export default () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route path="/edit" component={ToDo} />
+      <Route path="/:articleId" component={ToDo} />
     </Switch>
   </BrowserRouter>
 );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 export default class Home extends Component {
+  static propTypes = {}
+
   render() {
     return (
       <h1>Hello there</h1>
