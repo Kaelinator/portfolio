fe606924c78bca4c9ca65f05af44a36a761821db
Kaelinator <kirkkael@gmail.com>
2018-12-29 14:24:47 -0600

    trying out firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import * as firebase from 'firebase';
 
 import Home from './routes/Home';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyCbxy9RWIUIUckXpRPea0zwlg1drezBtHs',
+  authDomain: 'portfolio-5e3de.firebaseapp.com',
+  databaseURL: 'https://portfolio-5e3de.firebaseio.com',
+  projectId: 'portfolio-5e3de',
+  storageBucket: 'portfolio-5e3de.appspot.com',
+  messagingSenderId: '441789742914',
+});
 
 const ToDo = () => <h1>Under construction</h1>;
 
