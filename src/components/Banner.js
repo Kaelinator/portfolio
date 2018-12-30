c35485aaa91069bb14035e550622b0db8e1d2b07
Kaelinator <kirkkael@gmail.com>
2018-12-29 21:47:42 -0600

    Banner & posed

---
+++ src/components/Banner.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 440px;
+  height: 200px;
+  display: grid;
+  grid-template-rows: repeat(3, auto);
+`;
+
+const Name = styled.h1`
+  font-family: arial;
+  text-align: center;
+  font-size: 2em;
+`;
+
+export default class Banner extends Component {
+  render() {
+    return (
+      <Container>
+        <Name>Kael Kirk</Name>
+        <p>Heck</p>
+        <p>Heck!</p>
+      </Container>
+    );
+  }
+}
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
+import posed, { PoseGroup } from 'react-pose';
+import styled from 'styled-components';
+
+import { Desktop, Tablet, Mobile } from '../components/DeviceQueries';
+import Banner from '../components/Banner';
+
+const Item = posed.li();
+
+const List = styled.ul`
+  list-style: none;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  flex-direction: row;
+  display: flex;
+`;
+
+const Article = styled.div`
+  display: block;
+  border-radius: 4px;
+  background: red;
+  width: 200px;
+  height: 200px;
+  margin: 10px;
+`;
 
 export default class Home extends Component {
-  static propTypes = {}
+  state = {
+    articles: Array(50)
+      .fill(0)
+      .map((_, i) => ({
+        id: i,
+        body: () => ((i === 2) ? <Banner /> : <Article />),
+      })),
+  }
+
+  // constructor(props) {
+  //   super(props);
+  // }
 
   render() {
+    const { articles } = this.state;
     return (
-      <h1>Hello there</h1>
+      <>
+        <Desktop>
+          <List>
+            <PoseGroup>
+              {articles.map(({ id, body }) => <Item key={id}>{body()}</Item>)}
+            </PoseGroup>
+          </List>
+        </Desktop>
+        <Tablet><div style={{ background: 'green' }}>You are on a Tablet</div></Tablet>
+        <Mobile><div style={{ background: 'blue' }}>You are on a Mobile</div></Mobile>
+      </>
     );
   }
 }
