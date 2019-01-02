a38dbb76858660a0c67a6203b250856b292eca83
Kaelinator <kirkkael@gmail.com>
2019-01-01 21:11:04 -0600

    basic article styling

--- src/layouts/ArticleLayout.js
+++ src/layouts/ArticleLayout.js
@@ -1,17 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import styled from 'styled-components';
+
+import { Desktop, Tablet, Mobile } from './DeviceQueries';
+
+const WrapperLarge = styled.div`
+  display: grid;
+  grid-template-areas:
+    'left titl right'
+    'left tags right'
+    'left subt right'
+    'left text right'
+    'left foot right';
+  grid-template-columns: 3fr 5fr 3fr;
+`;
+
+const WrapperMedium = styled.div`
+  display: grid;
+  grid-template-areas:
+    'titl titl titl'
+    'tags tags tags'
+    'subt subt subt'
+    'left text right'
+    'left foot right';
+  grid-template-columns: 3fr 10fr 3fr;
+`;
+
+const FillWrapper = styled.div`
+  display: grid;
+  grid-template-areas:
+    'titl'
+    'tags'
+    'subt'
+    'text'
+    'foot';
+`;
+
 const ArticleLayout = (props) => {
   const { children } = props;
   return (
-    <div>
-      {children}
-    </div>
+    <>
+      <Desktop>
+        <WrapperLarge>{children}</WrapperLarge>
+      </Desktop>
+      <Tablet>
+        <WrapperMedium>{children}</WrapperMedium>
+      </Tablet>
+      <Mobile>
+        <FillWrapper>{children}</FillWrapper>
+      </Mobile>
+    </>
   );
 };
 
 ArticleLayout.propTypes = {
-  children: PropTypes.object.isRequired,
+  children: PropTypes.array.isRequired,
 };
 
 export default ArticleLayout;
--- src/routes/Article.js
+++ src/routes/Article.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import styled from 'styled-components';
+import Markdown from 'react-markdown';
+
 import ArticleLayout from '../layouts/ArticleLayout';
+import { Tag } from '../components/Tag';
+
+const Title = styled.h1`
+  font-size: 3em;
+  font-family: 'Times New Roman', Times;
+  margin-bottom: 0;
+  grid-area: titl;
+`;
+
+const Body = styled.div`
+  grid-area: text;
+`;
+
+const Subtitle = styled.h2`
+  grid-area: subt;
+  margin-top: 0;
+`;
+
+const Tags = styled.div`
+  grid-area: tags;
+`;
 
 export default class Article extends Component {
   static propTypes = {
     match: PropTypes.object.isRequired,
   }
 
+  state = {
+    markdown: Array(100).fill('This is my markdown!').join(' '),
+    tags: [
+      <Tag key={2} color="#22DDEE" accent="#00BBCC">Coding</Tag>,
+      <Tag key={3} color="#55EE22" accent="#33CC00">Creating</Tag>,
+    ],
+  }
+
   render() {
     const { match } = this.props;
+    const { markdown, tags } = this.state;
     return (
       <ArticleLayout>
-        <h1>{`Article ${match.params.articleId}`}</h1>
+        <Title>{`Article ${match.params.articleId}`}</Title>
+        <Subtitle>{`You had better believe that this is article ${match.params.articleId}`}</Subtitle>
+        <Tags>{tags}</Tags>
+        <Body><Markdown source={markdown} /></Body>
       </ArticleLayout>
     );
   }
