d53311f841097d4ac07e05d51d090eb917c5f74f
Kaelinator <kirkkael@gmail.com>
2019-01-09 22:37:42 -0600

    responsive EditLayout

--- src/routes/Edit/EditLayout.js
+++ src/routes/Edit/EditLayout.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import styled from 'styled-components';
 
-// import { Desktop } from '../../components/DeviceQueries';
+import { Desktop, Tablet, Mobile } from '../../components/DeviceQueries';
 
 const WrapperLarge = styled.div`
   width: 100vw;
@@ -16,11 +16,43 @@ const WrapperLarge = styled.div`
   grid-template-rows: auto 1fr;
 `;
 
+const WrapperMedium = styled.div`
+  width: 100vw;
+  display: grid;
+  grid-template-areas:
+    'status  status  '
+    'tags    articles'
+    'article article '
+    'body    body    '
+    'assets  assets  ';
+  grid-template-columns: 1fr 3fr;
+  grid-template-rows: auto auto auto 500px auto;
+`;
+
+const WrapperSmall = styled.div`
+  width: 100vw;
+  display: grid;
+  grid-template-areas:
+    'status'
+    'tags'
+    'articles'
+    'article'
+    'body'
+    'assets';
+  grid-template-rows: auto auto auto auto 500px auto;
+`;
+
 const EditLayout = ({ children }) => (
   <>
-    {/* <Desktop> */}
-    <WrapperLarge>{children}</WrapperLarge>
-    {/* </Desktop> */}
+    <Desktop>
+      <WrapperLarge>{children}</WrapperLarge>
+    </Desktop>
+    <Tablet>
+      <WrapperMedium>{children}</WrapperMedium>
+    </Tablet>
+    <Mobile>
+      <WrapperSmall>{children}</WrapperSmall>
+    </Mobile>
   </>
 );
 
